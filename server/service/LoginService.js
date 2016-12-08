fb6a911be95f7f8aca45437739c3f47b575b7494
wangchao <wangchao@youzan.com>
2016-12-08 15:15:23 +0800

    feat: 支持cdn打包

--- server/service/LoginService.js
+++ server/service/LoginService.js
@@ -1,8 +1,10 @@
 import randomName from 'random-name';
-import { Models } from '../model/model';
+import { Models, redis } from '../model/model';
 
 export default class LoginService {
   static async createRandomAccount() {
+    await redis.set(['some other key', 'some val']);
+
     return await Models.Account.create({
       username: randomName(),
       password: 123456,
