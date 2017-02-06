45add1679b676351df104150c58489560dfae374
wangchao <wangchao@youzan.com>
2017-02-06 14:30:02 +0800

    feat: 更新model的index文件

--- boilerplate/server/model/index.js
+++ boilerplate/server/model/index.js
@@ -1,1 +1,50 @@
-export { default as Account } from './Account';
+import Redis from 'redis';
+import coRedis from 'co-redis';
+import bunyan from 'bunyan';
+import Sequelize from 'sequelize';
+import {
+  mysql as mysqlConfig,
+  redis as redisConfig,
+} from '../config/index';
+
+const ENV = process.env.NODE_ENV;
+const Logger = bunyan.createLogger({ name: `SQL[${mysqlConfig[ENV].database}]` });
+
+const sequelize = new Sequelize(
+  mysqlConfig[ENV].database,
+  mysqlConfig[ENV].username,
+  mysqlConfig[ENV].password,
+  {
+    host: mysqlConfig[ENV].host,
+    dialect: 'mysql',
+    dialectOptions: {
+      charset: 'utf8mb4',
+    },
+    define: {
+      underscored: true,
+      underscoredAll: true,
+    },
+    logging: (() => {
+      const logger = Logger.child({ database: mysqlConfig[ENV].database });
+      return logger.info.bind(logger);
+    })(),
+    pool: {
+      max: 500,
+      min: 20,
+      idle: 10000,
+    },
+    timezone: '+08:00',
+    timestamps: true,
+  },
+);
+
+const rawRedis = Redis.createClient(redisConfig[ENV]);
+rawRedis.on('error', err => Logger.fatal(err, { database: 'redis' }));
+
+const redis = coRedis(rawRedis);
+
+const Models = {};
+
+Models.Account = require('./Account')(sequelize, Sequelize);
+
+export { sequelize, redis, Models };
