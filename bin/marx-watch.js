4de60e3f483e7672864c60627d4606ad026d158f
wangchao <wangchao@youzan.com>
2017-02-24 10:08:37 +0800

    fix: superman cannot find

--- bin/marx-watch.js
+++ bin/marx-watch.js
@@ -1,6 +1,5 @@
-const path  = require('path');
 const spawn = require('child_process').spawn;
 
-const superman = path.resolve(__dirname, '../node_modules/@youzan/superman/index.js');
+const superman = require.resolve('@youzan/superman');
 
 spawn(superman, ['build'], { stdio: 'inherit' });
