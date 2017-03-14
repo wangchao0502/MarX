a466300966c3f9c7acb0c9a344a46beeaf7b8d59
wangchao <wangchao@youzan.com>
2017-03-14 22:55:43 +0800

    feat: support error pass object

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -5,12 +5,15 @@ function errorMiddleware(msg) {
     ctx.redirect(msg);
   } else {
     let errorMsg = '';
+    let errorData = null;
     if (msg instanceof Error) {
       errorMsg = msg.message;
-    } else if (typeof msg === 'string' || typeof msg === 'object') {
+    } else if (typeof msg === 'string') {
       errorMsg = msg;
+    } else if (typeof msg === 'object') {
+      errorData = msg;
     }
-    ctx.body = { code: -1, msg: errorMsg };
+    ctx.body = { code: -1, msg: errorMsg, data: errorData };
   }
 }
 
