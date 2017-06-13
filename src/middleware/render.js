78c2a8e24938cccd46ae620083e46f9440ce2bbc
wangchao <wangchao@youzan.com>
2017-06-13 13:18:49 +0800

    feat: use another way to set global

--- src/middleware/render.js
+++ src/middleware/render.js
@@ -27,19 +27,13 @@ const createEnv = (opts) => {
 const renderMiddleware = (opts) => {
   // 创建Nunjucks的env对象:
   const env = createEnv(opts);
-
-  // load global values
-  Object.keys(opts.global || {}).forEach(key => {
-    env.addGlobal(key. opts.global[key]);
-  });
-
   const ext = opts.ext || '.njk';
   const data = opts.global || {};
   return async (ctx, next) => {
     // 给ctx绑定render函数:
     ctx.render = (view, model = {}) => {
       // 把render后的内容赋值给response.body:
-      ctx.response.body = env.render(view + ext, { ...ctx.state, ...model });
+      ctx.response.body = env.render(view + ext, { ...opts.global, ...ctx.state, ...model });
       // 设置Content-Type:
       ctx.response.type = 'text/html';
     };
