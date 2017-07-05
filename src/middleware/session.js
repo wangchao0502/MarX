8fcc46e953128aba2de053c2f362d9fa92947565
wangchao <wangchao@youzan.com>
2017-07-05 18:28:25 +0800

    fix: var name error

--- src/middleware/session.js
+++ src/middleware/session.js
@@ -5,7 +5,7 @@ import convert from 'koa-convert';
 
 const cwd  = process.cwd();
 const ENV  = process.env.NODE_ENV;
-const prodPath = ENV !== 'development' ? 'publish' : '';
+const prod = ENV !== 'development' ? 'publish' : '';
 
 const pkgJson     = require(path.join(cwd, prod, 'package.json'));
 const redisConfig = require(path.join(cwd, prod, 'server/config/redis'));
